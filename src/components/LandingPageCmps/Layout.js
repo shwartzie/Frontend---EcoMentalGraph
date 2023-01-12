6e1a88264565ced2543695574d373fe75981e0ea
Roni <ronnybusiness77@gmail.com>
2023-01-12 21:53:02 +0200

    fixed file paths css files

---
+++ src/components/LandingPageCmps/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavBar } from '../GlobalCmps/NavBar';
+export const Layout = () => {
+
+    return (
+        <header>
+            <NavBar />
+        </header>
+    );
+};

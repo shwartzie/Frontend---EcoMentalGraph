9ece3a2b3aa21e4f94624c60bfb8502a2d7a1c79
Roni <ronnybusiness77@gmail.com>
2023-01-12 20:06:38 +0200

    fixed axios path in http service

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,11 +1,7 @@
-import Axios from 'axios'
+import axios from './axios'
 
 const BASE_URL =
 	process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3030/api/'
-
-const axios = Axios.create({
-	withCredentials: true
-})
 
 export const httpService = {
 	get(endpoint, data) {
